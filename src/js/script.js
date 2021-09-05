3ec7b724ebd72e7dd90043c0b2b5044ace02b83c
Katarzyna Krysińska <katarzyna.krysinska@onet.pl>
2021-09-05 22:11:34 +0200

    Add images functionality

--- src/js/script.js
+++ src/js/script.js
@@ -98,6 +98,7 @@
 
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
 
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -180,22 +181,33 @@
           const option = param.options[optionId];
           console.log(optionId, option);
 
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          if(optionSelected) {
           // check if the option is not default
             if(!option.default == true) 
             // add option price to price variable
               price = price + option.price;
-            
+
           } else {
           // check if the option is default
             if(option.default == true) 
             // reduce price variable
               price = price - option.price;
           }
+
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          if(optionImage) {
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
         }
       }
-    
+
       //update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
 
