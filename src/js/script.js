be82e9642784f5772dcb437109d626948eb5019e
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-09-17 23:44:43 +0200

    Add initCart

--- src/js/script.js
+++ src/js/script.js
@@ -351,6 +351,14 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -361,6 +369,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
