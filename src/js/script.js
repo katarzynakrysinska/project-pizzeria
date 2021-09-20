9e38cf79b74ab985570550f6de57a7bca7205468
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-09-21 01:36:45 +0200

    Add remove code

--- src/js/script.js
+++ src/js/script.js
@@ -454,6 +454,7 @@
       // total price counting
       if (thisCart.totalNumber === 0){
         thisCart.totalPrice = 0;
+        thisCart.deliveryFee = 0;
       } else {
         thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
       }
@@ -481,6 +482,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
     }
 
     getElements(element){
@@ -506,6 +508,33 @@
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
 
+      });
+    }
+
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions(){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function(event){
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCartProduct.remove();
+
+        console.log('remove:', thisCartProduct.remove);
       });
     }
 
