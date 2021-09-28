7d010fd99db18adc344b366141054e68dfb913bb
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-09-29 00:15:45 +0200

    Files added

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,10 +2,13 @@ export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product', // CODE ADDED
+    bookingWidget: '#template-booking-widget', //NEW 10.4
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',  //NEW 10.4
+    booking: '.booking-wrapper',  //NEW 10.4
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -25,6 +28,15 @@ export const select = {
       input: 'input.amount', // CODE CHANGED
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
+    },
+    datePicker: { //NEW 10.4
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+    hourPicker: { //NEW 10.4
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
     },
   },
   // CODE ADDED START
@@ -47,6 +59,14 @@ export const select = {
     remove: '[href="#remove"]',
   },
   // CODE ADDED END
+  booking: { //NEW 10.4
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: { //NEW 10.4
+    links: '.main-nav a',
+  },
 };
   
 export const classNames = {
@@ -58,7 +78,17 @@ export const classNames = {
   cart: {
     wrapperActive: 'active',
   },
-// CODE ADDED END
+  // CODE ADDED END
+  booking: {  //NEW 10.4
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {  //NEW 10.4
+    active: 'active',
+  },
+  pages: {  //NEW 10.4
+    active: 'active',
+  }
 };
   
 export const settings = {
@@ -74,10 +104,26 @@ export const settings = {
   // CODE ADDED END
 
   //get data from API
-  db: {
+  db: {  //NEW 10.4
     url: '//localhost:3131',
     products: 'products',
-    orders: 'orders',
+    orders: 'orders',   // w pliku było 'order'
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+  },
+  hours: {  //NEW 10.4
+    open: 12,
+    close: 24,
+  },
+  datePicker: {  //NEW 10.4
+    maxDaysInFuture: 14,
+  },
+  booking: {  //NEW 10.4
+    tableIdAttribute: 'data-table',
   },
 };
   
@@ -86,4 +132,5 @@ export const templates = {
   // CODE ADDED START
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   // CODE ADDED END
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),//NEW 10.4
 };
