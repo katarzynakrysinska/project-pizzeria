49c2466c4efe0852e32707aae12f6cff9ac9dd73
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-10-15 12:59:33 +0200

    Change utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,3 @@
-
-
 const utils = {}; // eslint-disable-line no-unused-vars
 
 utils.createDOMFromHTML = function(htmlString) {
@@ -35,15 +33,6 @@ utils.serializeFormToObject = function(form){
     }
   }
   return output;
-};
-
-utils.convertDataSourceToDbJson = function(){
-  const productJson = [];
-  for(let key in dataSource.products){
-    productJson.push(Object.assign({id: key}, dataSource.products[key]));
-  }
-
-  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
 };
 
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
